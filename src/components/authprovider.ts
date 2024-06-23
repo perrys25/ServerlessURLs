210a835eca40d75ccb9b6558b3981d130db12756
Perry Sahnow <57735040+perrys25@users.noreply.github.com>
2024-06-23 12:55:24 -0400

    Rmemove edge runtime from authprovider

--- src/components/authprovider.ts
+++ src/components/authprovider.ts
@@ -1,8 +1,6 @@
 "use server"
 import {signIn} from "@/auth";
 
-export const runtime = 'edge'
-
 export default async function AuthProvider() {
     console.log("Calling AuthProvider with GitHub")
     await signIn("github")
--- src/components/sign-in.tsx
+++ src/components/sign-in.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import AuthProvider from "@/components/authprovider";
 
 export function SignIn() {
