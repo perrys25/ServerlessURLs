a82e95c3bd43b54aca80d777cd0068a8ee7374f2
Perry Sahnow <57735040+perrys25@users.noreply.github.com>
2024-06-24 19:53:35 -0400

    Unauthorized Page

--- src/app/links/[code]/page.tsx
+++ src/app/links/[code]/page.tsx
@@ -6,7 +6,23 @@ import {headers} from "next/headers";
 export default async function Home({params: {code}}: { params: { code: string } }): Promise<React.ReactNode> {
     const data = await getAnalytics(code);
     if (!data) {
-        return <InvalidCode/>
+        return (
+            <div className="flex-grow flex flex-col">
+                <div className="flex-grow"/>
+                <div className="my-auto flex flex-col">
+                    <div className="mx-auto w-max">
+                        <div
+                            className="bg-gray-100 shadow-lg rounded-2xl p-4 flex flex-col text-gray-900 w-[48rem]">
+                            <div className="px-6 py-4 space-y-4 md:px-8 w-full text-lg">
+                                <h1 className="text-2xl font-bold w-max mx-auto">You are not authorized to view that
+                                    link</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex-grow"/>
+            </div>
+        )
     }
     const headersList = headers();
     const host = headersList.get("host");
@@ -20,12 +36,15 @@ export default async function Home({params: {code}}: { params: { code: string } 
                     <div
                         className="bg-gray-100 shadow-lg rounded-2xl p-4 flex flex-col text-gray-900 w-[48rem]">
                         <div className="px-6 py-4 space-y-4 md:px-8 w-full text-lg">
-                            <h1 className="text-2xl font-bold w-max mx-auto">Link Details for <code className="bg-gray-300 rounded-md px-1">{code}</code></h1>
+                            <h1 className="text-2xl font-bold w-max mx-auto">Link Details for <code
+                                className="bg-gray-300 rounded-md px-1">{code}</code></h1>
                             <span className="flex flex-row">
-                                Shortened URL:&nbsp;<Link href={`${https ? "https" : "http"}://${host}/${code}`}><p className="text-blue-600 underline">{https ? "https" : "http"}://{host}/{code}</p></Link>
+                                Shortened URL:&nbsp;<Link href={`${https ? "https" : "http"}://${host}/${code}`}><p
+                                className="text-blue-600 underline">{https ? "https" : "http"}://{host}/{code}</p></Link>
                             </span>
                             <span className="flex flex-row">
-                                Original URL:&nbsp;<Link href={data.url}><p className="text-blue-600 underline">{data.url}</p></Link>
+                                Original URL:&nbsp;<Link href={data.url}><p
+                                className="text-blue-600 underline">{data.url}</p></Link>
                             </span>
                             <p>Total Clicks: {data.clicks}</p>
                         </div>
@@ -34,14 +53,6 @@ export default async function Home({params: {code}}: { params: { code: string } 
                 </div>
             </div>
             <div className="flex-grow"/>
-        </div>
-    )
-}
-
-function InvalidCode() {
-    return (
-        <div>
-            <h1>Invalid Code</h1>
         </div>
     )
 }
