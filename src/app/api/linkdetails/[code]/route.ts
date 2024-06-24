6251c52c240bfb6c23b152d58027c4559265c1e2
Perry Sahnow <57735040+perrys25@users.noreply.github.com>
2024-06-24 01:47:15 -0400

    No SQL checking

--- src/app/api/linkdetails/[code]/route.ts
+++ src/app/api/linkdetails/[code]/route.ts
@@ -1,6 +1,5 @@
 import {auth} from "@/auth";
 import {NextRequest} from "next/server";
-import SQL from '@nearform/sql'
 import {getRequestContext} from "@cloudflare/next-on-pages";
 
 export const runtime = 'edge'
@@ -19,11 +18,11 @@ export async function GET(req: NextRequest, {params}: { params: { code: string }
         return new Response(null, {status: 404})
     }
     console.log(`Redirects to ${url}`)
-    const query = SQL`
+    const query = `
         SELECT
             timestamp, blob1 AS change, blob2 AS code, blob3 AS referer
         FROM link_tracking
-        WHERE code = ${params.code}`.text
+        WHERE code = ${params.code}`
     const API = `https://api.cloudflare.com/client/v4/accounts/${process.env.CF_API_ID}/analytics_engine/sql`;
     const response = await fetch(API, {
         method: 'POST',
@@ -35,8 +34,8 @@ export async function GET(req: NextRequest, {params}: { params: { code: string }
     const text = await response.text();
     console.log(text)
     console.log(`Fetched ${response.status}`)
-    const result: { data: any[] } = JSON.parse(text);
-    const numClicks = result.data.length;
+    const result: { rows: number } = JSON.parse(text);
+    const numClicks = result.rows;
     console.log(`Found ${numClicks} clicks`)
     return new Response(JSON.stringify({url, clicks: numClicks}), {status: 200})
 }
