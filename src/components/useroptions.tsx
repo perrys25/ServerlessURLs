bafd77f9f294ef35e3120447e4ee7199253855e0
Perry Sahnow <57735040+perrys25@users.noreply.github.com>
2024-06-24 15:07:15 -0400

    Fix logging out

--- src/components/useroptions.tsx
+++ src/components/useroptions.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import {signOut} from "@/auth";
 
-async function MenuItem({name, link, className}: { name: string, link: string | (() => Promise<void>), className?: string}) {
+async function MenuItem({name, link, className}: {
+    name: string,
+    link: string | (() => Promise<void>),
+    className?: string
+}) {
     if (typeof link === "string") {
         return (
-            <a href={link} className={`block px-4 py-2 text-gray-900 hover:bg-gray-200 rounded-md ${className}`}>{name}</a>
+            <a href={link}
+               className={`block px-4 py-2 text-gray-900 hover:bg-gray-200 rounded-md ${className}`}>{name}</a>
         )
     }
     return (
         // <a onClick={async () => await link()} className="block px-4 py-2 text-gray-900 hover:bg-gray-200">{name}</a>
-        <form action={async () => {
-            "use server";
-            await link()
-        }}  className={`block px-4 py-2 text-gray-900 hover:bg-gray-200 rounded-md ${className}`}>
-            <button type="submit">{name}</button>
+        <form action={link}>
+            <button type="submit" className={`block px-4 py-2 text-gray-900 hover:bg-gray-200 rounded-md w-full text-left ${className}`}>{name}</button>
         </form>
     )
 }
@@ -25,7 +27,10 @@ export default async function UserOptions() {
                 className="bg-gray-100 shadow-lg rounded-2xl flex flex-col text-gray-900 max-w-md sm:min-w-96">
                 <div className="p-2 space-y-2 md:p-4">
                     <MenuItem name={"My Links"} link={"/links"}/>
-                    <MenuItem name={"Log Out"} link={async () => signOut()}/>
+                    <MenuItem name={"Log Out"} link={async () => {
+                        "use server";
+                        await signOut();
+                    }}/>
                 </div>
             </div>
         </>
