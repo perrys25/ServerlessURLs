47fcafc500bf7e0cba39ddd795ca30683a7bfee7
Perry Sahnow <57735040+perrys25@users.noreply.github.com>
2024-06-24 21:45:47 -0400

    Add google as OAuth Provider

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,17 +1,18 @@
 import GitHub from "next-auth/providers/github"
 import Discord from "next-auth/providers/discord"
 import Keycloak from "next-auth/providers/keycloak";
+import Google from "next-auth/providers/google";
 import type {NextAuthConfig} from "next-auth"
 import {IconProp} from "@fortawesome/fontawesome-svg-core";
 import {faKey} from "@fortawesome/free-solid-svg-icons";
-import {faDiscord, faGithub} from "@fortawesome/free-brands-svg-icons";
+import {faDiscord, faGithub, faGoogle} from "@fortawesome/free-brands-svg-icons";
 import {Provider} from "@auth/core/providers";
 
 export let providers: {
     provider: Provider
     icon: IconProp,
     color: string,
-}[] =  []
+}[] = []
 
 console.log(process.env.TEST_AUTH === 'true')
 
@@ -23,15 +24,21 @@ if (process.env.TEST_AUTH === 'true') {
     })
 } else {
     providers.push({
-        provider: GitHub,
-        icon: faGithub,
-        color: "#333",
-    },
+            provider: GitHub,
+            icon: faGithub,
+            color: "#333",
+        },
         {
             provider: Discord,
             icon: faDiscord,
             color: "#5865F2",
-        })
+        },
+        {
+            provider: Google,
+            icon: faGoogle,
+            color: "#4285F4",
+        }
+    )
 }
 
 export const providerData = providers.map(({provider, ...args}) => {
